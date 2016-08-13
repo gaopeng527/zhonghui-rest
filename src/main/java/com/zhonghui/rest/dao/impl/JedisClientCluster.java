9ce9f3eb4750bba8d4eeb16b3926f5b0f9d83734
gaopeng527 <1259728004@qq.com>
2016-08-13 10:15:04 +0800

    配置一个jedis的接口及其实现类来统一操作单机和集群版的Redis

---
+++ src/main/java/com/zhonghui/rest/dao/impl/JedisClientCluster.java
@@ -0,0 +1,60 @@
+package com.zhonghui.rest.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zhonghui.rest.dao.JedisClient;
+
+import redis.clients.jedis.JedisCluster;
+/**
+ * jedis集群版实现类
+ * @author DELL
+ *
+ */
+public class JedisClientCluster implements JedisClient {
+
+	@Autowired
+	private JedisCluster jedisCluster;
+	
+	@Override
+	public String get(String key) {
+		String value = jedisCluster.get(key);
+		return value;
+	}
+
+	@Override
+	public String set(String key, String value) {
+		String result = jedisCluster.set(key, value);
+		return result;
+	}
+
+	@Override
+	public String hget(String hkey, String key) {
+		String value = jedisCluster.hget(hkey, key);
+		return value;
+	}
+
+	@Override
+	public long hset(String hkey, String key, String value) {
+		long result = jedisCluster.hset(hkey, key, value);
+		return result;
+	}
+
+	@Override
+	public long incr(String key) {
+		long result = jedisCluster.incr(key);
+		return result;
+	}
+
+	@Override
+	public long expire(String key, int seconds) {
+		long result = jedisCluster.expire(key, seconds);
+		return result;
+	}
+
+	@Override
+	public long ttl(String key) {
+		long result = jedisCluster.ttl(key);
+		return result;
+	}
+
+}
