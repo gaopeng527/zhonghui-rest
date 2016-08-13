36b53e5005f675f7cff0b8f9931fc140bb8fc6ca
gaopeng527 <1259728004@qq.com>
2016-08-13 10:39:44 +0800

    在内容管理中使用Redis缓存

--- src/main/java/com/zhonghui/rest/service/impl/ContentServiceImpl.java
+++ src/main/java/com/zhonghui/rest/service/impl/ContentServiceImpl.java
@@ -2,13 +2,17 @@ package com.zhonghui.rest.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.huizhong.mapper.TbContentMapper;
 import com.huizhong.pojo.TbContent;
 import com.huizhong.pojo.TbContentExample;
 import com.huizhong.pojo.TbContentExample.Criteria;
+import com.zhonghui.common.utils.JsonUtils;
+import com.zhonghui.rest.dao.JedisClient;
 import com.zhonghui.rest.service.ContentService;
 /**
  * 内容管理
@@ -20,15 +24,39 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${INDEX_CONTENT_REDIS_KEY}")
+	private String INDEX_CONTENT_REDIS_KEY;
 	
 	@Override
 	public List<TbContent> getContentList(long contentCid) {
+		// 从缓存中取内容
+		try {
+			String result = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, contentCid+"");
+			if (!StringUtils.isBlank(result)){
+				// 把字符串转换成list
+				List<TbContent> resultList = JsonUtils.jsonToList(result, TbContent.class);
+				return resultList;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		// 根据内容分类id查询内容列表
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andCategoryIdEqualTo(contentCid);
 		// 执行查询
 		List<TbContent> list = contentMapper.selectByExample(example);
+		
+		// 向缓存中添加内容
+		try {
+			// 把list转换成字符串
+			String cacheString = JsonUtils.objectToJson(list);
+			jedisClient.hset(INDEX_CONTENT_REDIS_KEY, contentCid+"", cacheString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
